eecb36ee4cc0e0d02ceb812efbb26c044b0c398f
webcuser <97110714+webcuser@users.noreply.github.com>
2026-07-11 11:18:03 +0200

    webcuser/taskflow-webapp

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,9 +4,10 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  root: './',
   build: {
     rollupOptions: {
-      input: 'index.html'
+      input: './index.html'
     }
   }
 });
